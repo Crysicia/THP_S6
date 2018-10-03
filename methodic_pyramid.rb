630cf1098f808de48d82a05e67e58e17d5afea16
Crysicia <lucienmontmartre@yahoo.fr>
2018-10-03 13:49:37 +0200

    Hey I'm methodic now

---
+++ methodic_pyramid.rb
@@ -0,0 +1,23 @@
+def ask_how_many_floors
+    puts "hOI boi, I'm making a pyramid, can you give me a number between 1 and 25 ?"
+    floors = gets.chomp.to_i
+end
+
+def print_pyramid (floors)
+    if floors <= 25 && floors >= 1
+        hash = "#"
+        floors.times do
+            puts hash
+            hash << "#"
+        end
+    else
+        puts "I said between 1 and 25...\nSo let me ask again"
+        perform
+    end
+end
+
+def perform
+    print_pyramid(ask_how_many_floors)
+end
+
+perform
