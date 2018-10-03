e68463bb52f04a5f8643ae520d5f240d4d2c72f1
Crysicia <lucienmontmartre@yahoo.fr>
2018-10-03 15:00:06 +0200

    UNLOCK THE SECRET FACILITY

--- password-o-tron.rb
+++ password-o-tron.rb
@@ -7,7 +7,7 @@ end
 def password_verification
     puts "Type your password again"
     print "> "
-    if gets.chomp == @user_password
+    if gets.chomp === @user_password
         puts "Your passwords are matching."
     else
         puts "The entries are not the same, please try again."
@@ -15,9 +15,24 @@ def password_verification
     end
 end
 
+def connect(password)
+    if password === @user_password
+        puts "You are now connected to our uber-secret facility !"
+    else
+        puts "Wrong password, expect to be raided by the FBI in the next 5 to 10 minutes."
+    end
+end
+
+def connection
+    puts "Please enter your password"
+    print "> "
+    connect(gets.chomp)
+end
+
 def perform 
-    define_password 
+    define_password
     password_verification
+    connection
 end
 
 perform
