6830a5607fea9c3c45bf0898847d2968ba3b95bb
Crysicia <lucienmontmartre@yahoo.fr>
2018-10-09 16:40:11 +0200

    Added trading_with_lehman.rb, rubocop friendly

--- trading_with_lehman.rb
+++ trading_with_lehman.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 def trader_du_dimanche(arr)
-  
+  hash = {}
+  arr.each_with_index do |a, b|
+    for i in b..arr.length - 1
+      hash["#{b},#{i}"] = arr[i] - a
+    end
+  end
+  hash.max_by{ |_k, v| v }[0].split(",").map{ :to_i }
 end
-
-
-p trader_du_dimanche([17,3,6,9,15,8,6,1,10])
