56181ff6741d243d54593dbc6563f95ad3c42be2
Crysicia <lucienmontmartre@yahoo.fr>
2018-10-17 16:22:31 +0200

    Removed json.rb

--- lib/json.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-json = File.read('../files/input.json')
-obj = JSON.parse(json)
-
-hash = {"President"=>"Alan Isaac", "CEO"=>"David Richardson", "India"=>["Sachin Tendulkar", "Virender Sehwag", "Gautam Gambhir"], "Srilanka"=>["Lasith Malinga", "Angelo Mathews", "Kumar Sangakkara"], "England"=>["Alastair Cook", "Jonathan Trott", "Kevin Pietersen"]}
-
-p obj
-p hash.to_json
