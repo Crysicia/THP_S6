aad89d674aa6adff21d6ccc08de5eb545b2abc41
Crysicia <lucienmontmartre@yahoo.fr>
2018-11-05 11:14:56 +0100

    Add routes to login, logout, signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  
+  get 'login', to: 'session#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
 end
