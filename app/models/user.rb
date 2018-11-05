1edec9af10e3bffc949d32fcd56465bce8239b38
Crysicia <lucienmontmartre@yahoo.fr>
2018-11-05 09:48:30 +0100

    Add validation and secure_password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true
+  has_secure_password
 end
