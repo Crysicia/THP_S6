d645dba9dea2841737bc2468f1c77d73ea28a42b
ssimonlp <simon.lepavec@gmail.com>
2018-11-05 20:49:40 +0100

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
-  
+  def show
+    if logged_in?
+     @user = User.find(params[:id])
+     else
+      flash[:error] = "You must be logged in to access this page"
+      redirect_to login_path
+    end
+
+    if session[:user_id].to_i != params[:id].to_i
+      flash[:error] = "You're not allowed to access this page"
+      redirect_to root_path
+    end
+  end
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,7 +27,16 @@ class UsersController < ApplicationController
       redirect_to signup_path
     end
   end
-  
+  def edit
+   @user = User.find(session[:id])
+  end
+   def update
+    @user = User.find(session[:id])
+    @user.first_name = 
+    redirect_to user_path
+   end
+
+
   def destroy
     @user.destroy
     flash[:info] = "Account successfully deleted, sorry to see you go."
