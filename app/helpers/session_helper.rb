e16634dfde333654e03a1bf7f64be83ba8e7e0ef
Crysicia <lucienmontmartre@yahoo.fr>
2018-11-05 10:11:20 +0100

    Add session helper

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,20 @@
 module SessionHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def current_user
+    if (user_id = session[:user_id])
+      @currnet_user ||= User.find_by(id: user_id)
+    end
+  end
+  
+  def log_out
+    ession.delete(:user_id)
+    redirect_to root_path
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
 end
